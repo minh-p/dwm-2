afe6b3cfade82f39f792198d83c0177c3bc86d4a
minh-p <minhisverynormal@gmail.com>
2022-04-09 14:54:02 -0700

    colorscheme management in dwm

---
+++ schemes/default-dmenu.h
@@ -0,0 +1,4 @@
+#define dmenu_nb "#222222"
+#define dmenu_nf "#bbbbbb"
+#define dmenu_sb "#005577"
+#define dmenu_sf "#eeeeee"
---
+++ schemes/default.h
@@ -0,0 +1,5 @@
+static const char *colors[][3] = {
+	/* fg, bg, border */
+	[SchemeNorm] = {"#bbbbbb", "#222222", "#444444"},
+	[SchemeSel] = {"#eeeeee", "#005577", "#005577"}
+};

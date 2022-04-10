39a81eafa0f069bc29217f8c6c4f42a01ee4898c
minh-p <minhisverynormal@gmail.com>
2022-04-09 18:26:42 -0700

    font and fixborders patch

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = {"Font Awesome 6 Free:size=8", "monospace:size=10"};
 static const char dmenufont[]       = "monospace:size=10";
 #include "./schemes/default.h"
 #include "./schemes/default-dmenu.h"

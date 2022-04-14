4959fb85b8137cb38d5748322e73d5673f217994
minh-p <minhisverynormal@gmail.com>
2022-04-13 19:19:07 -0700

    dwm colors default is bestgit add .

--- config.h
+++ config.h
@@ -30,7 +30,7 @@ static const Rule rules[] = {
 	{ "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "qutebrowser",  NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "discord",  NULL,       NULL,       1 << 2,			0,           -1 },
-	{ "mpv",	  NULL,       NULL,       1 << 8,			0,           -1 },
+	{ "mpv",	  NULL,       NULL,       1 << 7,			0,           -1 },
 };
 
 /* layout(s) */

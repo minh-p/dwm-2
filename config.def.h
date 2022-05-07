e4443646d0d9dcbeefffc536951a704a6ee00046
minh-p <minhisverynormal@gmail.com>
2022-05-07 15:44:41 -0700

    config.def.h up-to-date

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {"Font Awesome 6 Free:size=8", "monospace:size=10"};
 static const char dmenufont[]       = "monospace:size=10";
-#include "./schemes/default.h"
+#include "./schemes/default-custom.h"
 #include "./schemes/default-dmenu.h"
 
 /* commands */

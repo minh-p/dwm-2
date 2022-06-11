5d8caadb98b7d404fcbc01a07df3e979173472ae
minh-p <minhisverynormal@gmail.com>
2022-06-11 09:55:34 -0700

    added centeredmaster patch

--- config.def.h
+++ config.def.h
@@ -56,7 +56,9 @@ static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "",      tile },    /* first entry is default */
 	{ "",      NULL },    /* no layout function means floating behavior */
-	{ "[]",      monocle },
+	{ "",    monocle },
+	{ "",    centeredmaster},
+	{ "",    centeredfloatingmaster},
 };
 
 /* key definitions */
@@ -90,6 +92,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
